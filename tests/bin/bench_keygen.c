6bba619fce350bae01b7273dc2bfe79cec7bbff6
conduition <conduition@proton.me>
2025-11-14 06:42:39 +0000

    remove unnecessary print of pkRoot

--- tests/bin/bench_keygen.c
+++ tests/bin/bench_keygen.c
@@ -19,8 +19,6 @@ int main() {
   uint8_t pkSeed[SLHVK_N] = {16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
   uint8_t skSeed[SLHVK_N] = {0x00, 0x00, 0x11, 0x22, 0xff, 0x29, 0x99, 0x90,
                              0x01, 0x02, 0x03, 0x04, 0xaa, 0xbb, 0xcc, 0xdd};
-  uint8_t pkRoot[SLHVK_N] = {0};
-
 
   #define KEYGEN_RUNS 512
   const uint8_t* skSeeds[KEYGEN_RUNS];
@@ -49,11 +47,11 @@ int main() {
 
   printf("took %.2f ms per key gen\n", time_delta_ms(start, end) / (double) nRuns);
 
-  memcpy(pkRoot, pkRoots[0], SLHVK_N);
-  printf("generated pk_root: ");
-  for (int j = 0; j < SLHVK_N; j++)
-    printf("%02x", pkRoot[j]);
-  printf("\n");
+  // memcpy(pkRoot, pkRoots[0], SLHVK_N);
+  // printf("generated pk_root: ");
+  // for (int j = 0; j < SLHVK_N; j++)
+  //   printf("%02x", pkRoot[j]);
+  // printf("\n");
 
 
 cleanup:
