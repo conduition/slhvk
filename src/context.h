b674f42dcb510c0b6532b18f1d63fa2c32a9ac26
conduition <conduition@proton.me>
2025-11-17 08:07:22 +0000

    separate file for signing

--- src/context.h
+++ src/context.h
@@ -1,10 +1,43 @@
+#pragma once
 #include <stdint.h>
 #include <vulkan/vulkan.h>
 
+#include "slhvk.h"
+
 #define N SLHVK_N
+
+#define WOTS_TIPS_PRECOMPUTE_PIPELINE_DESCRIPTOR_COUNT 2
+#define XMSS_LEAVES_PRECOMPUTE_PIPELINE_DESCRIPTOR_COUNT 3
+#define XMSS_MERKLE_SIGN_PIPELINE_DESCRIPTOR_COUNT 4
+#define WOTS_SIGN_PIPELINE_DESCRIPTOR_COUNT 3
+#define FORS_LEAVES_GEN_PIPELINE_DESCRIPTOR_COUNT 4
+#define FORS_MERKLE_SIGN_PIPELINE_DESCRIPTOR_COUNT 4
 
 #define KEYGEN_PIPELINE_DESCRIPTOR_COUNT 4
 #define VERIFY_PIPELINE_DESCRIPTOR_COUNT 2
+
+// Buffer sizes
+#define WOTS_CHAIN_BUFFER_SIZE (N * SLHVK_WOTS_CHAIN_COUNT * SLHVK_XMSS_LEAVES * SLHVK_HYPERTREE_LAYERS)
+#define XMSS_NODES_BUFFER_SIZE (N * SLHVK_XMSS_LEAVES * SLHVK_HYPERTREE_LAYERS)
+#define XMSS_MESSAGES_BUFFER_SIZE (sizeof(uint32_t) * SLHVK_WOTS_CHAIN_COUNT * SLHVK_HYPERTREE_LAYERS)
+#define FORS_MESSAGE_BUFFER_SIZE (sizeof(uint32_t) * SLHVK_FORS_TREE_COUNT)
+#define FORS_NODES_BUFFER_SIZE (N * SLHVK_FORS_TREE_COUNT * SLHVK_FORS_LEAVES_COUNT)
+#define FORS_ROOTS_BUFFER_SIZE (N * SLHVK_FORS_TREE_COUNT)
+#define FORS_PUBKEY_STAGING_BUFFER_SIZE (sizeof(uint32_t) * SLHVK_WOTS_CHAIN_COUNT)
+
+typedef struct CommonSigningInputs {
+  // The SHA256 state after absorbing the `pk_seed` and padding.
+  uint32_t sha256State[8];
+
+  // Secret seed from the private key.
+  uint32_t skSeed[SLHVK_HASH_WORDS];
+
+  // adrs[1:4]
+  uint64_t treeAddress;
+
+  // the index of the layer 0 keypair to be used for signing the message.
+  uint32_t signingKeypairAddress;
+} CommonSigningInputs;
 
 typedef struct SlhvkContext_T {
   VkInstance instance;
---
+++ src/signing.c
@@ -0,0 +1,237 @@
+#include <stdint.h>
+#include <string.h>
+#include <vulkan/vulkan.h>
+
+#include "context.h"
+#include "hashing.h"
+#include "sha256.h"
+
+static void prepstate(ShaContext* shaCtx, const uint8_t pkSeed[N]) {
+  uint8_t block[64] = {0};
+  memcpy(block, pkSeed, N);
+  sha256_init(shaCtx);
+  sha256_update(shaCtx, block, 64);
+}
+
+int slhvkSignPure(
+  SlhvkContext ctx,
+  const uint8_t skSeed[N],
+  const uint8_t skPrf[N],
+  const uint8_t pkSeed[N],
+  const uint8_t pkRoot[N],
+  const uint8_t addrnd[N],
+  const uint8_t* contextString,
+  uint8_t contextStringSize,
+  const uint8_t* rawMessage,
+  size_t rawMessageSize,
+  uint8_t signatureOutput[SLHVK_SIGNATURE_SIZE]
+) {
+  // Deterministic mode
+  if (addrnd == NULL) addrnd = pkSeed;
+
+  uint8_t randomizer[N];
+  slhvkMessagePrf(
+    skPrf,
+    addrnd,
+    contextString,
+    contextStringSize,
+    rawMessage,
+    rawMessageSize,
+    randomizer
+  );
+
+  uint32_t forsIndices[SLHVK_FORS_TREE_COUNT];
+  uint64_t treeAddress;
+  uint32_t signingKeypairAddress;
+  slhvkDigestAndSplitMsg(
+    randomizer,
+    pkSeed,
+    pkRoot,
+    contextString,
+    contextStringSize,
+    rawMessage,
+    rawMessageSize,
+    forsIndices,
+    &treeAddress,
+    &signingKeypairAddress
+  );
+
+  VkQueue primaryQueue;
+  vkGetDeviceQueue(ctx->primaryDevice, ctx->primaryDeviceQueueFamily, 0, &primaryQueue);
+
+  VkQueue secondaryQueue;
+  if (ctx->secondaryDevice == ctx->primaryDevice) {
+    secondaryQueue = primaryQueue;
+  } else {
+    vkGetDeviceQueue(ctx->secondaryDevice, ctx->secondaryDeviceQueueFamily, 0, &secondaryQueue);
+  }
+
+  // We create two fences to await the final outputs on each device.
+  VkFence primaryFence = NULL;
+  VkFence secondaryFence = NULL;
+
+  VkFenceCreateInfo fenceCreateInfo = { .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO };
+  int err = vkCreateFence(ctx->primaryDevice, &fenceCreateInfo, NULL, &primaryFence);
+  if (err) goto cleanup;
+  err = vkCreateFence(ctx->secondaryDevice, &fenceCreateInfo, NULL, &secondaryFence);
+  if (err) goto cleanup;
+
+  // Prehash the pk_seed value.
+  ShaContext shaCtxInitial;
+  prepstate(&shaCtxInitial, pkSeed);
+
+  // Write inputs straight to the device local buffers if we can.
+  VkDeviceMemory primaryInputsMemory = (ctx->primaryDeviceLocalMemoryFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)
+    ? ctx->primaryInputsBufferDeviceLocalMemory
+    : ctx->primaryInputsBufferHostVisibleMemory;
+  VkDeviceMemory secondaryInputsMemory = (ctx->secondaryDeviceLocalMemoryFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)
+    ? ctx->secondaryInputsBufferDeviceLocalMemory
+    : ctx->secondaryInputsBufferHostVisibleMemory;
+
+  VkDeviceMemory memories[2] = { primaryInputsMemory, secondaryInputsMemory };
+  VkDevice       devices[2]  = { ctx->primaryDevice, ctx->secondaryDevice };
+
+  for (int i = 0; i < 2; i++) {
+    CommonSigningInputs* mapped = NULL;
+    err = vkMapMemory(
+      devices[i],
+      memories[i],
+      0, // offset
+      sizeof(CommonSigningInputs),
+      0, // flags
+      (void**) &mapped
+    );
+    if (err) goto cleanup;
+
+    memcpy(&mapped->sha256State[0], shaCtxInitial.state, sizeof(uint32_t) * 8);
+
+    // Copy the skSeed into a big-endian encoded u32 array
+    for (size_t i = 0; i < SLHVK_HASH_WORDS; i++) {
+      size_t i4 = i * sizeof(uint32_t);
+      mapped->skSeed[i] = ((uint32_t) skSeed[i4] << 24) | ((uint32_t) skSeed[i4 + 1] << 16) |
+                          ((uint32_t) skSeed[i4 + 2] << 8) | (uint32_t) skSeed[i4 + 3];
+    }
+    mapped->treeAddress = treeAddress;
+    mapped->signingKeypairAddress = signingKeypairAddress;
+    vkUnmapMemory(devices[i], memories[i]);
+  }
+
+  // Submit the XMSS precomputation shaders right away, because they take the most runtime.
+  VkSubmitInfo submitInfo = {
+    .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+    .commandBufferCount = 1,
+    .pCommandBuffers = &ctx->primaryHypertreePresignCommandBuffer,
+  };
+  err = vkQueueSubmit(primaryQueue, 1, &submitInfo, primaryFence);
+  if (err) goto cleanup;
+
+  // Write the FORS indices to the FORS message buffer so it will be signed.
+  VkDeviceMemory forsMessageMemory = (ctx->secondaryDeviceLocalMemoryFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)
+    ? ctx->secondaryForsMessageBufferDeviceLocalMemory
+    : ctx->secondaryForsMessageBufferHostVisibleMemory;
+  uint32_t* mappedForsMessage = NULL;
+  err = vkMapMemory(
+    ctx->secondaryDevice,
+    forsMessageMemory,
+    0, // offset
+    FORS_MESSAGE_BUFFER_SIZE,
+    0, // flags
+    (void**) &mappedForsMessage
+  );
+  if (err) goto cleanup;
+  memcpy(mappedForsMessage, forsIndices, FORS_MESSAGE_BUFFER_SIZE);
+  vkUnmapMemory(ctx->secondaryDevice, forsMessageMemory);
+
+  // Submit the FORS command buffer to the secondary device.
+  submitInfo.pCommandBuffers = &ctx->secondaryForsCommandBuffer;
+  err = vkQueueSubmit(secondaryQueue, 1, &submitInfo, secondaryFence);
+  if (err) goto cleanup;
+
+  // Wait for secondary (FORS) shaders to complete
+  err = vkWaitForFences(ctx->secondaryDevice, 1, &secondaryFence, VK_TRUE, 100e9);
+  if (err) goto cleanup;
+
+  // Read the FORS roots output.
+  uint8_t* mappedForsRoots = NULL;
+  err = vkMapMemory(
+    ctx->secondaryDevice,
+    ctx->secondaryForsRootsBufferMemory,
+    0, // offset
+    FORS_ROOTS_BUFFER_SIZE,
+    0, // flags
+    (void**) &mappedForsRoots
+  );
+  if (err) goto cleanup;
+  uint8_t forsRoots[FORS_ROOTS_BUFFER_SIZE];
+  memcpy(forsRoots, mappedForsRoots, FORS_ROOTS_BUFFER_SIZE);
+  vkUnmapMemory(ctx->secondaryDevice, ctx->secondaryForsRootsBufferMemory);
+
+  uint32_t wotsMessage[SLHVK_WOTS_CHAIN_COUNT];
+  slhvkHashForsRootsToWotsMessage(
+    forsRoots,
+    treeAddress,
+    signingKeypairAddress,
+    &shaCtxInitial,
+    wotsMessage
+  );
+
+  // Copy the encoded FORS pubkey WOTS message to the primary device staging buffer.
+  uint32_t* mappedWotsMessage = NULL;
+  err = vkMapMemory(
+    ctx->primaryDevice,
+    ctx->primaryForsPubkeyStagingBufferMemory,
+    0, // offset
+    FORS_PUBKEY_STAGING_BUFFER_SIZE,
+    0, // flags
+    (void**) &mappedWotsMessage
+  );
+  if (err) goto cleanup;
+  for (int i = 0; i < SLHVK_WOTS_CHAIN_COUNT; i++) {
+    mappedWotsMessage[i] = wotsMessage[i];
+  }
+  vkUnmapMemory(ctx->primaryDevice, ctx->primaryForsPubkeyStagingBufferMemory);
+
+  // Wait for the XMSS precomputation shaders to finish. These take up the majority of runtime.
+  err = vkWaitForFences(ctx->primaryDevice, 1, &primaryFence, VK_TRUE, 100e9);
+  if (err) goto cleanup;
+
+  // Reset this fence so we can reuse it for the final submission.
+  err = vkResetFences(ctx->primaryDevice, 1, &primaryFence);
+  if (err) goto cleanup;
+
+  // Submit and await the final WOTS signing shader.
+  submitInfo.pCommandBuffers = &ctx->primaryHypertreeFinishCommandBuffer;
+  err = vkQueueSubmit(primaryQueue, 1, &submitInfo, primaryFence);
+  if (err) goto cleanup;
+  err = vkWaitForFences(ctx->primaryDevice, 1, &primaryFence, VK_TRUE, 100e9);
+  if (err) goto cleanup;
+
+  // Copy the randomizer to the signature output
+  memcpy(signatureOutput, randomizer, N);
+
+  // Copy the FORS signature to the output pointer
+  uint8_t forsSig[SLHVK_FORS_SIGNATURE_SIZE];
+  VkDeviceMemory forsSigMemory = (ctx->secondaryDeviceLocalMemoryFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)
+    ? ctx->secondaryForsSignatureBufferDeviceLocalMemory
+    : ctx->secondaryForsSignatureBufferHostVisibleMemory;
+  uint8_t* mappedSignature = NULL;
+  err = vkMapMemory(ctx->secondaryDevice, forsSigMemory, 0, SLHVK_FORS_SIGNATURE_SIZE, 0, (void**) &mappedSignature);
+  if (err) goto cleanup;
+  memcpy(&signatureOutput[N], mappedSignature, SLHVK_FORS_SIGNATURE_SIZE);
+  memcpy(forsSig, mappedSignature, SLHVK_FORS_SIGNATURE_SIZE);
+  vkUnmapMemory(ctx->secondaryDevice, forsSigMemory);
+
+  // Copy the hypertree signature to the output pointer
+  VkDeviceMemory hypertreeSigMemory = (ctx->primaryDeviceLocalMemoryFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)
+    ? ctx->primaryHypertreeSignatureBufferDeviceLocalMemory
+    : ctx->primaryHypertreeSignatureBufferHostVisibleMemory;
+  err = vkMapMemory(ctx->primaryDevice, hypertreeSigMemory, 0, SLHVK_HYPERTREE_SIGNATURE_SIZE, 0, (void**) &mappedSignature);
+  if (err) goto cleanup;
+  memcpy(&signatureOutput[N + SLHVK_FORS_SIGNATURE_SIZE], mappedSignature, SLHVK_HYPERTREE_SIGNATURE_SIZE);
+  vkUnmapMemory(ctx->primaryDevice, hypertreeSigMemory);
+
+cleanup:
+  vkDestroyFence(ctx->primaryDevice, primaryFence, NULL);
+  vkDestroyFence(ctx->secondaryDevice, secondaryFence, NULL);
+  return err;
+}
